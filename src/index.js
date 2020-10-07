e386c05f703953eb35e62d2723b0539f5a08cbde
Joe Binney <joseph.binney@gmail.com>
2020-10-07 01:29:40 -0500

    Set up rendering frame with placeholder welcome message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("joebinney.com")
+import { Application, Text, TilingSprite } from 'pixi.js'
+
+const app = new Application({
+    width: 480,
+    height: 270
+})
+app.view.id = 'appCanvas'
+document.body.appendChild(app.view)
+
+const welcomeMessage = new Text('joebinney.com', {
+    fontFamily: 'monospace',
+    fontSize: 50,
+    fill: 0xffffff
+})
+welcomeMessage.anchor.set(0.5, 0.5)
+welcomeMessage.position.set(app.view.width/2, app.view.height/2)
+app.stage.addChild(welcomeMessage)
